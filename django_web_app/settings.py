91889e7d663ba154f5edb8408159605aeb7ab7ce
Joseph <joebiju8@gmail.com>
2023-03-13 03:48:37 +0000

    added ip

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['64.226.77.41']
 
 
 # Application definition
