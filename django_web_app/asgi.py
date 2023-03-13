d93807cc62ed017bf1f7d0a03b4ae9f12cea5f35
Joseph <joebiju8@gmail.com>
2023-03-13 08:24:44 +0000

    asgi change

--- django_web_app/asgi.py
+++ django_web_app/asgi.py
@@ -8,13 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_web_app.settings')
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_web_app.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
